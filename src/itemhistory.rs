b85ff44c77608e88bc02975378efe1a9a64b938f
Luukas Pörtfors <lajp@iki.fi>
2023-07-31 15:24:01 +0300

    feat(itemhistory): Make itemhistory into a HashMap

--- src/itemhistory.rs
+++ src/itemhistory.rs
@@ -2,57 +2,41 @@ use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
 use dashmap::DashMap;
-use serenity::prelude::TypeMapKey;
 
 #[derive(Debug, Clone)]
 pub struct ItemHistory {
-    // (item_id, site_id, timestamp)
-    items: Vec<(i64, i32, i64)>,
+    // (item_id, site_id), timestamp
+    items: HashMap<(i64, i32), i64>,
 }
 
 // (user_id, delivery_method) => ItemHistory
 pub type ItemHistoryStorage = Arc<DashMap<(u64, i32), Arc<Mutex<ItemHistory>>>>;
 
-impl TypeMapKey for ItemHistory {
-    type Value = Arc<Mutex<ItemHistory>>;
-}
-
 impl ItemHistory {
     pub fn new() -> ItemHistory {
-        Self { items: vec![] }
+        Self {
+            items: HashMap::new(),
+        }
     }
 
     pub fn add_item(&mut self, id: i64, site_id: i32, timestamp: i64) {
         if !self.contains(id, site_id) {
             debug!("Adding id: {},{}, timestamp: {}", id, site_id, timestamp);
-            self.items.push((id, site_id, timestamp))
+            self.items.insert((id, site_id), timestamp);
         }
     }
 
     pub fn contains(&self, id: i64, site_id: i32) -> bool {
-        self.items
-            .iter()
-            .any(|(iid, sid, _)| iid == &id && sid == &site_id)
+        self.items.contains_key(&(id, site_id))
     }
 
     pub fn purge_old(&mut self) {
-        self.items = self
-            .items
-            .iter()
-            .filter(|(_, _, timestamp)| timestamp > &(chrono::Local::now().timestamp() - 1000))
-            .map(|t| t.to_owned())
-            .collect();
+        self.items
+            .retain(|(_, _), timestamp| timestamp > &mut (chrono::Local::now().timestamp() - 1000));
     }
 
     pub fn extend(&mut self, other: &Self) {
-        let mut map: HashMap<(i64, i32), i64> = other
-            .items
-            .clone()
-            .into_iter()
-            .map(|(iid, sid, t)| ((iid, sid), t))
-            .collect();
-        map.extend(self.items.iter().map(|(iid, sid, t)| ((*iid, *sid), *t)));
-        self.items = map.iter().map(|((iid, sid), t)| (*iid, *sid, *t)).collect();
+        self.items.extend(other.items.iter());
         self.purge_old()
     }
 }
